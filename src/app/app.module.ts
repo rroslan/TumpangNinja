6487dbcba3a1bf6bbf6b05d053d636de9dc6d8c1
Roslan Ramli <rroslan@gmail.com>
2017-03-09 09:56:58 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,11 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { AngularFireModule } from 'angularfire2';
 
 export const  firebaseConfig = {
-  apiKey: "AIzaSyBYdvfieXUp5-nCnB0yOBZzEXCXyLFXVa0",
+  apiKey: "Your API KEY",
   authDomain: "tumpang.firebaseapp.com",
   databaseURL: "https://tumpang.firebaseio.com",
   storageBucket: "firebase-tumpang.appspot.com",
-  messagingSenderId: "468167798410"
+  messagingSenderId: "Your message id"
 };
 @NgModule({
   declarations: [
